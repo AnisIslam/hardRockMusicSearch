42cd72ab9b65d8a49644443c1781346a0d0e698c
Anis Islam <manisul1997bd@gmail.com>
2021-02-14 19:17:15 +0600

    spinner and enter button search functionalities added

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const searchSongs = () => {
+// const searchSongs = () => {
+//     const searchText = document.getElementById('search-field').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchText}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displaySongs(data.data))
+
+// }
+
+
+//Search by Enter key functionalities
+var searchBtn = document.getElementById("search-button");
+document.getElementById("search-field")
+    .addEventListener("keypress", function (event) {
+       
+        if (event.key == 'Enter'){
+            searchBtn.click();
+
+
+        }
+    });
+
+const searchSongs = async () => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySongs(data.data))
+    toggleSpinner();
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySongs(data.data);
+
+    } catch (error) {
+        displayError("Sorry Something wrong!");
+
+    }
 
 }
 
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
-
+    songContainer.innerHTML = '';
     songs.forEach(song => {
         const songDiv = document.createElement('div');
         songDiv.className = "single-result row align-items-center my-3 p-3";
@@ -30,11 +59,38 @@ const displaySongs = songs => {
         </div>
         `
         songContainer.appendChild(songDiv);
+        toggleSpinner();
     })
 
 }
 
-const getLyric = (artist,title) => {
-    console.log(artist);
+const getLyric = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayLyrics(data.lyrics);
+    // document.getElementById('song-lyrics').innerHTML = '';
 
+}
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+
+    lyricsDiv.innerText = lyrics;
+}
+
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error;
+}
+
+const toggleSpinner = () => {
+    const spinner = document.getElementById('loading-spinner');
+    // if (show) {
+    //     spinner.classList.remove("d-none");
+    // }
+    spinner.classList.toggle('d-none');
+    // else {
+    //     spinner.classList.add('d-none');
+
+    // }
 }
